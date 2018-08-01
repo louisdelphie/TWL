5eaee5b4a48cb0247f8ce0a4c95f1129f17d8241
Louis Delphie <louisdelphie@msn.com>
2018-08-01 15:43:18 -0400

    init

---
+++ TWL/Thomas.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "PlayableCharacter.h"
+
+class Thomas : public PlayableCharacter
+{
+public:
+	// A constructor specific to Thomas
+	Thomas();
+
+	// The overridden input handler for Thomas
+	bool virtual handleInput();
+
+};
+
